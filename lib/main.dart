6f547360e007ee69b3adde46c90d091ec2d62714
Prateek Thakur <prateekthakur272@gmail.com>
2023-07-29 17:23:39 +0530

    cupertino app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_sqlite/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       title: 'Flutter SQLite',
-      home: const Home(),
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
+      home: Home(),
+      theme: CupertinoThemeData(
+        primaryColor: CupertinoColors.activeGreen,
       ),
     );
   }
