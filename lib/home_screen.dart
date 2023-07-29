845687815319050a997b938cf7bc182bac4b2b3a
Prateek Thakur <prateekthakur272@gmail.com>
2023-07-29 11:27:38 +0530

    adds data to database

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/database.dart';
+import 'package:flutter_sqlite/modals.dart';
 import 'package:flutter_sqlite/view_data.dart';
 
 class Home extends StatelessWidget {
@@ -7,6 +9,10 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _name = TextEditingController();
+    final _email = TextEditingController();
+    final _phone = TextEditingController();
+
     return Scaffold(
       appBar: const CupertinoNavigationBar(
         middle: Text('Flutter SQLite',
@@ -15,23 +21,26 @@ class Home extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(24),
         children: [
-          const CupertinoTextField(
-            padding: EdgeInsets.all(16),
+          CupertinoTextField(
+            padding: const EdgeInsets.all(16),
             placeholder: 'Name',
+            controller: _name,
           ),
           const SizedBox(
             height: 16,
           ),
-          const CupertinoTextField(
-            padding: EdgeInsets.all(16),
+          CupertinoTextField(
+            padding: const EdgeInsets.all(16),
             placeholder: 'Email',
+            controller: _email,
           ),
           const SizedBox(
             height: 16,
           ),
-          const CupertinoTextField(
-            padding: EdgeInsets.all(16),
+          CupertinoTextField(
+            padding: const EdgeInsets.all(16),
             placeholder: 'Phone',
+            controller: _phone,
           ),
           const SizedBox(
             height: 16,
@@ -39,25 +48,53 @@ class Home extends StatelessWidget {
           CupertinoButton.filled(
               child: const Text('Add'),
               onPressed: () {
-                showCupertinoDialog(
-                    context: context,
-                    builder: (context) {
-                      return CupertinoAlertDialog(
-                        title: const Text('Added'),
-                        content: const Text('Data added successfully'),
-                        actions: [
-                          CupertinoButton(
-                              child: const Text('Done'),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              }),
-                        ],
-                      );
-                    });
+                final name = _name.text.trim();
+                final email = _email.text.trim();
+                final phone = _phone.text.trim();
+
+                if (name.isNotEmpty && email.isNotEmpty && phone.isNotEmpty) {
+                  DatabaseHelper.add(Person(name, email, phone)).then((value) {
+                    showCupertinoDialog(
+                        context: context,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            title: const Text('Added'),
+                            content: Text('$name added successfully'),
+                            actions: [
+                              CupertinoButton(
+                                  child: const Text('Done'),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  }),
+                            ],
+                          );
+                        });
+                  });
+                  _name.clear();
+                  _email.clear();
+                  _phone.clear();
+                } else {
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          title: const Text('Failed'),
+                          content:
+                              const Text('Can\'t add data with empty values'),
+                          actions: [
+                            CupertinoButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Close')),
+                          ],
+                        );
+                      });
+                }
               }),
           CupertinoButton(
               child: const Text('View persisted data'),
-              onPressed: () {
+              onPressed: () async {
                 Navigator.push(context,
                     CupertinoPageRoute(builder: (context) => const ViewData()));
               })
